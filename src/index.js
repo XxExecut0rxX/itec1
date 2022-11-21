e8958d59825c5d933a446e92b7c4c07b95c65f30
XxExecut0rxX <alva200rey@gmail.com>
2022-11-21 02:14:30 +0000

    including hbs handlebars code base and express urlunencoded

--- src/index.js
+++ src/index.js
@@ -3,19 +3,33 @@ const app = express();
 const port = 3000;
 
 const morgan = require('morgan');
+const express_hbs = require('express-handlebars');
+// import { engine } from 'express-handlebars';
+const path = require('path');
 
 //settings
 // app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('.hbs', express_hbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir : path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+
+app.set('view engine', '.hbs');
 
 // middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // global variables
 
 // routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.use(require('./routes/index.js'));
 
 // public
 
