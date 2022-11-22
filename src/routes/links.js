d3f0c5004cfa70a57b3c435bbcf8e5d51cb143dc
XxExecut0rxX <alva200rey@gmail.com>
2022-11-22 00:05:57 +0000

    working on admins page and hbs variables

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const pool = require('../database');
 
 router.get('/add', (req, res) => {
-    res.send('Form');
+    res.render('links/add_item', { title: 'Add new User', style: '/css/styles_dashboard.css'});
 });
 
 module.exports = router;
