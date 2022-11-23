f2fc71ec40da02770a99f1e13645be4d58016e91
XxExecut0rxX <alva200rey@gmail.com>
2022-11-23 02:19:33 +0000

    adding edit and delete buttons on the items list page

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,14 +5,18 @@ const pool = require('../database');
 
 router.get('/add', async (req, res) => {
     const item_ids = await pool.query('SELECT id FROM producto');
-    const last_id = item_ids[item_ids.length - 1].id;
+    var last_id
+    if(item_ids.length > 0)
+        last_id = item_ids[item_ids.length - 1].id;
+    else 
+        last_id = 0;
     const next_id = last_id + 1;
     console.log(last_id);
-    res.render('links/add_item', { title: 'Add new User', style: '/css/styles_dashboard.css', next_id: next_id});
+    res.render('links/add_item', { title: 'Add new User', style: '/css/styles_dashboard.css', next_id: next_id,});
 });
 
 router.post('/add', async (req, res) => {
-    // console.log(req.body);<
+    // console.log(req.body);
 
     const {id, name, price, stock} = req.body;
     const newItem = {
@@ -24,13 +28,26 @@ router.post('/add', async (req, res) => {
     console.log(newItem);
     await pool.query('INSERT INTO producto set ?', [newItem]);
     // await console.log(pool.query('SELECT * FROM producto'));
-    res.send('resc');
+    res.redirect('/links');
 });
 
 router.get('/', async (req, res) => {
     const catalog_list = await pool.query('SELECT * FROM producto');
-    console.log(catalog_list);
-    res.send('lists');
+    // console.log(catalog_list);
+    res.render('links/item_list', {title: 'Lista de Productos', style: 'css/styles_market.css', list: catalog_list, img_c: '/catalog_imgs/arduino.jpg'});
+});
+
+router.get('/delete/:id', async (req, res) => {
+    // console.log(req.params.id);
+    const { id } = req.params;
+    await pool.query('DELETE FROM producto WHERE id = ?', [id]);
+    res.redirect('/links');
+})
+
+router.get('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    console.log(id);
+    res.send('received');
 });
 
 module.exports = router;
